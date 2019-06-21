62fd18ef044c00d365bcd99c30f889ed27bb94d2
Jeremy Shipman <jeremy@burnbright.net>
2019-06-21 15:11:36 +1200

    Enable addresses export endpoint to be accessed via basic auth (ideally over HTTPS)

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # DispatchIT Shop Order Exporter
 
-For New Zealand Couriers.
+Provides an endpoint for fetching files to use with a New Zealand Couriers printer.
 
-Write a desktop script to call yoursite.com/dispatchitexporter, and then download everything from
-yoursite/assets/_dispatchitexports to the local folder for importing into dispatchit.
+Write a desktop script to call `yoursite.com/dispatchitexporter`, and then download everything from
+`yoursite/assets/_dispatchitexports` to the local folder for importing into dispatchit. This could be done via ssh connection. If you use this approach, it is recommended that you include `.htaccess` config to close the folder off from the web.
+
+Alternatively, you can prevent writing output to the assets folder by adding the query param `yoursite.com/dispatchitexporter?nofile=true`.
+
+Adding `yoursite.com/dispatchitexporter?dryrun=true` will both prevent files being written, and orders from being updated to mark them as having been exported for printing.
+
+## Authentication
+
+Requests can be authenticated via [Basic Auth](https://en.wikipedia.org/wiki/Basic_access_authentication), but *Please Note:* you should do basic auth over HTTPS to avoid authentication details from being snooped.
+
+Create a new Member and add them to a Group with the permission "Access to address export API";
+
+Authorise requests by providing the username and password credentials Basic Auth header.
--- code/DispatchITExporter.php
+++ code/DispatchITExporter.php
@@ -1,85 +1,134 @@
 <?php
-class DispatchITExporter extends Controller{
 
-	static $url_segment = 'dispatchitexporter';
+/**
+ * Export addresses for printing
+ */
+class DispatchITExporter extends Controller implements PermissionProvider{
 
-	static $allowed_actions_disabled = array(
-		'index' => 'ADMIN',
-		'export' => 'ADMIN',
-	);
+	public static $url_segment = 'dispatchitexporter';
 
-	function init(){
+	public function init(){
 		parent::init();
-		if(!(Director::isDev() || Director::is_cli() || Permission::check("ADMIN")))
-			return Security::permissionFailure($this);
+		BasicAuth::requireLogin("Address exporting.", "Show_Address_Export", false);
 	}
 	
-	function Link($action = null,$id = null){
-		return Controller::join_links(self::$url_segment,$action,$id);
+	public function Link($action = null, $id = null){
+		return Controller::join_links(self::$url_segment, $action, $id);
 	}
-	
-	function index(){
+
+	public function index(){
 		return $this->export();
 	}
 
-	function export(){
-		$orders = Order::get()
-					->innerJoin("Address","\"Order\".\"ShippingAddressID\" = \"Address\".\"ID\"")
-					->filter("SentToDispatchIT", 0)
-					->filter("Status",Order::config()->placed_status)
-					->filter("Address.Country", "NZ")
-					->sort(array("Placed" => "ASC", "Created" => "ASC"));
+	public function export(){
+		$dryRun = (boolean)$this->request->getVar("dryrun");
+		$noFile = (boolean)$this->request->getVar("nofile");
+		$orders = $this->unShippedOrders();	
 		$output = "";
 		if($orders->exists()){
-			foreach($orders as $order){
-				$address = $order->getShippingAddress();
-				$name = $address->Company; //TODO: company
-				if(!$name || $name == ""){
-					$name = $order->Name;
+			$body = $this->createDispatchBody($orders);
+			try {
+				if (!$dryRun && !$noFile) {
+					$this->writeExportFile($body);
 				}
-				$line = array(
-					$order->ID,			//ConsignmentNumber	char(12)	Yes	Tracking number.  Must be unique.
-					$order->MemberID, //CustomerID			char(20)	Blank if ommitted
-					$name,				//CompanyName			char(40)	Yes
-					$address->Address,	//Address1			char(40)	Yes
-					$address->AddressLine2,	//Address2		char(40)	Yes
-					$address->Suburb,	//Address3				char(40)	Blank if ommitted
-					$address->State,	//Address4				char(40)	Blank if ommitted
-					$address->City,		//Address5			char(40)	Must be valid suburb/city/town.
-					"",					//CustOrderRef			char(30)	Blank if ommitted
-					"0",					//Carrier				smallint	Yes	0 = NZ Couriers
-					"1",					//CostCentre			smallint	Yes	1 = Primary cost centre.
-					"",					//PhoneSTD				char(4)	Blank if ommitted
-					$address->Phone,	//PhoneNumber			char(20)	Blank if ommitted
-					"",					//FaxSTD					char(4)	Blank if ommitted
-					$order->Fax,		//FaxNumber				char(20)	Blank if ommitted
-					$order->Email,		//EmailAddress			char(255)Blank if ommitted
-					"1",					//EmailAddressSend	bit		TRUE or FALSE
-					"",					//EmailAddress2		char(255)Blank if ommitted
-					"0",					//EmailAddress2Send	bit		TRUE or FALSE
-					"",					//EmailAddress3		char(255)Blank if ommitted
-					"0"					//EmailAddress3Send	bit		TRUE or FALSE;
-				);
-				$output .= implode("\t",$line)."\n";
-				$order->SentToDispatchIT = true;
-				$order->write();
-			}
-			//store output in a local file, then output the contents, or a link to the file
-				//name format: DDcxxxxxxx.TXT
-			$filename = "DDc".uniqid()."_".date('YmdHis').".txt";
-			$exportpath = ASSETS_PATH."/_dispatchitexports";
-			if(!is_dir($exportpath)){
-				mkdir($exportpath);
-			}
-			if(file_put_contents($exportpath."/$filename",$output) === false){
-				$output = "failed to save file";
+				if (!$dryRun) {
+					foreach($orders as $order){
+						$order->SentToDispatchIT = true;
+						$order->write();
+					}
+				}
+				$output = $body;
+			} catch (Exception $e) {
+				SS_Log::log($e->getMessage(), SS_Log::ERR);
+				$this->httpError(500, "Failed to save file");
+				return;
 			}
 		}else{
-			$output = "no new orders";
+			$this->httpError(204, "No new orders found");
+			return;
 		}
 		header('Content-type: text/plain');
+		// TODO: make sure responses aren't cached
 		echo $output;
 		die();
+	}
+
+	protected function unShippedOrders() {
+		return Order::get()
+			->innerJoin("Address", "\"Order\".\"ShippingAddressID\" = \"Address\".\"ID\"")
+			->filter("SentToDispatchIT", 0)
+			->filter("Status", Order::config()->placed_status)
+			->filter("Address.Country", "NZ")
+			->sort(array(
+				"Placed" => "ASC",
+				"Created" => "ASC"
+			));
+	}
+
+	protected function createDispatchITLine($order) {
+		$address = $order->getShippingAddress();
+		$name = $address->Company; //TODO: company
+		if(!$name || $name == ""){
+			$name = $order->Name;
+		}
+		return array(
+			$order->ID,			//ConsignmentNumber	char(12)	Yes	Tracking number.  Must be unique.
+			$order->MemberID, 	//CustomerID			char(20)	Blank if ommitted
+			$name,				//CompanyName			char(40)	Yes
+			$address->Address,	//Address1			char(40)	Yes
+			$address->AddressLine2,	//Address2		char(40)	Yes
+			$address->Suburb,	//Address3				char(40)	Blank if ommitted
+			$address->State,	//Address4				char(40)	Blank if ommitted
+			$address->City,		//Address5			char(40)	Must be valid suburb/city/town.
+			"",					//CustOrderRef			char(30)	Blank if ommitted
+			"0",				//Carrier				smallint	Yes	0 = NZ Couriers
+			"1",				//CostCentre			smallint	Yes	1 = Primary cost centre.
+			"",					//PhoneSTD				char(4)	Blank if ommitted
+			$address->Phone,	//PhoneNumber			char(20)	Blank if ommitted
+			"",					//FaxSTD					char(4)	Blank if ommitted
+			$order->Fax,		//FaxNumber				char(20)	Blank if ommitted
+			$order->Email,		//EmailAddress			char(255)Blank if ommitted
+			"1",				//EmailAddressSend	bit		TRUE or FALSE
+			"",					//EmailAddress2		char(255)Blank if ommitted
+			"0",				//EmailAddress2Send	bit		TRUE or FALSE
+			"",					//EmailAddress3		char(255)Blank if ommitted
+			"0"					//EmailAddress3Send	bit		TRUE or FALSE;
+		);
+	}
+
+	protected function createDispatchBody($orders) {
+		$output = "";
+		foreach($orders as $order){
+			$line = $this->createDispatchITLine($order);
+			$output .= implode("\t", $line)."\n";
+		}
+		return $output;
+	}
+
+	/**
+	 * Store output in a local file, then output the contents, or a link to the file
+	 * name format: DDcxxxxxxx.TXT
+	 */
+	protected function writeExportFile($body) {
+		$filename = "DDc".uniqid()."_".date('YmdHis').".txt";
+		$exportpath = ASSETS_PATH."/_dispatchitexports";
+		if(!is_dir($exportpath) && mkdir($exportpath) === false) {
+			throw new Exception("Couldn't make $exportpath directory");
+		}
+		$filePath = $exportpath."/$filename";
+		if(file_put_contents($filePath, $body) === false){
+			throw new Exception("Could not write file to $filePath");
+		}
+		return $filePath;
+	}
+
+	public function providePermissions() {
+		return array(
+			"Show_Address_Export" => array(
+				'name' => 'Access to address export API',
+				'category' => 'Shop'
+			)
+		);
 	}
 
 }
